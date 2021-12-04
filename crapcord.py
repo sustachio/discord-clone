b9c04630fedf4417feb0ba4347e5621c743b3176
sustachio <sustachio@gmail.com>
2021-12-03 23:02:21 -0600

    update 2: scrool bar+text color

--- crapcord.py
+++ crapcord.py
@@ -9,6 +9,7 @@ app.config['SECRET_KEY'] = '292ef1b0413859709s4570746a11e58d'
 def home():
     return render_template('home.html')
 
+"""
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
@@ -22,7 +23,7 @@ def register():
 def login():
     form = LoginForm()
     return render_template('login.html', title='Login', form=form)
-
+"""
 #runs the app w/ debug (auto update)
 if __name__ == '__main__':
     app.run(debug=True, port=8089)
